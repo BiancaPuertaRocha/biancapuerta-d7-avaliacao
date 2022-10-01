e8303e5454988947c25a987a9aca6a0efbf429f0
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-09-30 21:06:05 -0300

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+
+using login_sys.Data;
+using login_sys.Models;
+
+namespace login_sys.Controllers
+{
+    internal class UserController
+    {
+        private Context _context;
+        public UserController(Context context)
+        {
+            this._context = context;
+        }
+
+        public User GetLogin(string? user, string? pass)
+        {
+            var _users = _context.Users;
+            if (_users != null)
+            {
+                return _users.AsNoTracking()
+                .Where(x => user == x.Login)
+                .Where(x => pass == x.Password).FirstOrDefault();
+
+            }
+            else
+            {
+                throw new Exception("No Users found");
+            }
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace login_sys.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+       
+        public string Password { get; set; }
+    }
+}
